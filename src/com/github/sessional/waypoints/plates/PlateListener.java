77db9a702d9b95ed09cf968e431f27e20fc0405c
Andrew <andrew.kruse@blackhole.com>
2013-07-01 22:02:49 -0500

    Added source code
    Added source code for a plates module.
    This module teleports you using pressure plates when used in conjuction
    with Waypoints.Signs (However, Signs should not be necessary - this
    should work either way.)

---
+++ src/com/github/sessional/waypoints/plates/PlateListener.java
@@ -0,0 +1,100 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.sessional.waypoints.plates;
+
+import com.github.sessional.waypoints.WpsPlugin;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+/**
+ *
+ * @author Andrew
+ */
+class PlateListener implements Listener {
+
+    WpsPlatesPlugin plugin;
+
+    public PlateListener(WpsPlatesPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void OnPlateStep(PlayerInteractEvent event) {
+        if (event.getAction() != Action.PHYSICAL)
+            return;
+        if (event.getClickedBlock().getType() == Material.WOOD_PLATE || event.getClickedBlock().getType() == Material.STONE_PLATE) {
+            Player p = event.getPlayer();
+            Location plateLocation = event.getClickedBlock().getLocation();
+            int x = plateLocation.getBlockX();
+            int y = plateLocation.getBlockY();
+            int z = plateLocation.getBlockZ();
+            World w = plateLocation.getWorld();
+
+            Location infront = new Location(w, x + 1, y, z);
+            Location behind = new Location(w, x - 1, y, z);
+            Location right = new Location(w, x, y, z + 1);
+            Location left = new Location(w, x, y, z - 1);
+            Location above = new Location(w, x, y + 1, z);
+            Location below = new Location(w, x, y - 2, z);
+
+            if (infront.getBlock().getType() == Material.WALL_SIGN || infront.getBlock().getType() == Material.SIGN_POST) {
+                Sign s = (Sign) infront.getBlock().getState();
+                handleWarp(p, s);
+            } else if (behind.getBlock().getType() == Material.WALL_SIGN || behind.getBlock().getType() == Material.SIGN_POST) {
+                Sign s = (Sign) behind.getBlock().getState();
+                handleWarp(p, s);
+            } else if (right.getBlock().getType() == Material.WALL_SIGN || right.getBlock().getType() == Material.SIGN_POST) {
+                Sign s = (Sign) right.getBlock().getState();
+                handleWarp(p, s);
+            } else if (left.getBlock().getType() == Material.WALL_SIGN || left.getBlock().getType() == Material.SIGN_POST) {
+                Sign s = (Sign) left.getBlock().getState();
+                handleWarp(p, s);
+            } else if (above.getBlock().getType() == Material.WALL_SIGN || above.getBlock().getType() == Material.SIGN_POST) {
+                Sign s = (Sign) above.getBlock().getState();
+                handleWarp(p, s);
+            } else if (below.getBlock().getType() == Material.WALL_SIGN || below.getBlock().getType() == Material.SIGN_POST) {
+                Sign s = (Sign) below.getBlock().getState();
+                handleWarp(p, s);
+            }
+        }
+    }
+
+    public void handleWarp(Player p, Sign s) {
+
+        for (int i = 0; i < s.getLines().length - 1; i++) {
+            if (s.getLine(i).equals("Waypoint:") || s.getLine(i).equals("§aWaypoint:")) {
+                String wp = s.getLine(i + 1);
+                WpsPlugin plug = (WpsPlugin) plugin.getServer().getPluginManager().getPlugin("Waypoints");
+                if (plugin.isBukkitPermissions()) {
+                    if (p.hasPermission("waypoints.basic.go") || p.hasPermission("waypoints.plate.go")) {
+                        if (!s.getLine(i).equals("§aWaypoint:")) {
+                            s.setLine(i, "§aWaypoint:");
+                        }
+                        plug.getCommandHandler().doGo(p, new String[]{
+                            wp
+                        });
+                    } else {
+                        p.sendMessage("You do not have permission to pressure plate waypoint.");
+                    }
+                } else {
+                    if (!s.getLine(i).equals("§aWaypoint:")) {
+                        s.setLine(i, "§aWaypoint:");
+                        s.update();
+                    }
+                    plug.getCommandHandler().doGo(p, new String[]{
+                        wp
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ src/com/github/sessional/waypoints/plates/WpsPlatesPlugin.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.sessional.waypoints.plates;
+
+import java.io.File;
+import java.util.logging.Logger;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Andrew
+ */
+public class WpsPlatesPlugin extends JavaPlugin
+{
+    
+    private static final Logger log = Logger.getLogger("Minecraft");
+    private String version = "0.1.1";
+    private File configFile;
+    private boolean bukkitPermissions;
+    
+    @Override
+    public void onEnable()
+    {
+        configFile = new File("./plugins/Waypoints.Plates/config.yml");
+        if (!configFile.exists())
+        {
+            this.saveDefaultConfig();
+        }
+        
+        if (this.getConfig().getBoolean("bukkitPermissions") == true)
+        {
+            bukkitPermissions = true;
+        } else
+        {
+            bukkitPermissions = false;
+        }
+        
+        getServer().getPluginManager().registerEvents(new PlateListener(this), this);
+    }
+    
+    public boolean isBukkitPermissions()
+    {
+        return bukkitPermissions;
+    }
+    
+    @Override
+    public void onDisable()
+    {
+        
+    }
+}
