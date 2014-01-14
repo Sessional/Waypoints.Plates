024805d64e8c17a973e7ce6a2f99ca6ff5c0f1c8
Andrew <andrew.kruse@blackhole.com>
2014-01-14 09:54:41 -0600

    Costing enabled
    Fixes to be closer to intended since the double teleporting issue has
    been fixed.

--- src/waypoints/plates/PlateListener.java
+++ src/waypoints/plates/PlateListener.java
@@ -18,20 +18,25 @@ import org.bukkit.event.player.PlayerInteractEvent;
  *
  * @author Andrew
  */
-class PlateListener implements Listener {
+class PlateListener implements Listener
+{
 
     WpsPlatesPlugin plugin;
 
-    public PlateListener(WpsPlatesPlugin plugin) {
+    public PlateListener(WpsPlatesPlugin plugin)
+    {
         this.plugin = plugin;
     }
 
     @EventHandler
-    public void OnPlateStep(PlayerInteractEvent event) {
-        if (event.getAction() != Action.PHYSICAL) {
+    public void OnPlateStep(PlayerInteractEvent event)
+    {
+        if (event.getAction() != Action.PHYSICAL)
+        {
             return;
         }
-        if (event.getClickedBlock().getType() == Material.WOOD_PLATE || event.getClickedBlock().getType() == Material.STONE_PLATE) {
+        if (event.getClickedBlock().getType() == Material.WOOD_PLATE || event.getClickedBlock().getType() == Material.STONE_PLATE)
+        {
             Player p = event.getPlayer();
             Location plateLocation = event.getClickedBlock().getLocation();
             int x = plateLocation.getBlockX();
@@ -45,68 +50,133 @@ class PlateListener implements Listener {
             Location above = new Location(w, x, y + 1, z);
             Location below = new Location(w, x, y - 2, z);
 
-            if (infront.getBlock().getType() == Material.WALL_SIGN || infront.getBlock().getType() == Material.SIGN_POST) {
+            if (infront.getBlock().getType() == Material.WALL_SIGN || infront.getBlock().getType() == Material.SIGN_POST)
+            {
                 Sign s = (Sign) infront.getBlock().getState();
                 if (handleWarp(p, s))
+                {
                     event.setCancelled(true);
+                }
                 return;
             }
-            if (behind.getBlock().getType() == Material.WALL_SIGN || behind.getBlock().getType() == Material.SIGN_POST) {
+            if (behind.getBlock().getType() == Material.WALL_SIGN || behind.getBlock().getType() == Material.SIGN_POST)
+            {
                 Sign s = (Sign) behind.getBlock().getState();
                 if (handleWarp(p, s))
+                {
                     event.setCancelled(true);
+                }
                 return;
             }
-            if (right.getBlock().getType() == Material.WALL_SIGN || right.getBlock().getType() == Material.SIGN_POST) {
+            if (right.getBlock().getType() == Material.WALL_SIGN || right.getBlock().getType() == Material.SIGN_POST)
+            {
                 Sign s = (Sign) right.getBlock().getState();
                 if (handleWarp(p, s))
+                {
                     event.setCancelled(true);
+                }
                 return;
             }
-            if (left.getBlock().getType() == Material.WALL_SIGN || left.getBlock().getType() == Material.SIGN_POST) {
+            if (left.getBlock().getType() == Material.WALL_SIGN || left.getBlock().getType() == Material.SIGN_POST)
+            {
                 Sign s = (Sign) left.getBlock().getState();
                 if (handleWarp(p, s))
+                {
                     event.setCancelled(true);
+                }
                 return;
             }
-            if (above.getBlock().getType() == Material.WALL_SIGN || above.getBlock().getType() == Material.SIGN_POST) {
+            if (above.getBlock().getType() == Material.WALL_SIGN || above.getBlock().getType() == Material.SIGN_POST)
+            {
                 Sign s = (Sign) above.getBlock().getState();
                 if (handleWarp(p, s))
+                {
                     event.setCancelled(true);
+                }
                 return;
             }
-            if (below.getBlock().getType() == Material.WALL_SIGN || below.getBlock().getType() == Material.SIGN_POST) {
+            if (below.getBlock().getType() == Material.WALL_SIGN || below.getBlock().getType() == Material.SIGN_POST)
+            {
                 Sign s = (Sign) below.getBlock().getState();
                 if (handleWarp(p, s))
+                {
                     event.setCancelled(true);
+                }
                 return;
             }
         }
     }
 
-    public boolean handleWarp(Player p, Sign s) {
+    public boolean handleWarp(Player p, Sign s)
+    {
 
-        for (int i = 0; i < s.getLines().length - 1; i++) {
-            if (s.getLine(i).equals("Waypoint:") || s.getLine(i).equals("§aWaypoint:")) {
+        for (int i = 0; i < s.getLines().length - 1; i++)
+        {
+            if (s.getLine(i).equals("Waypoint:") || s.getLine(i).equals("§aWaypoint:"))
+            {
                 String wp = s.getLine(i + 1);
-                if (plugin.isBukkitPermissions()) {
-                    if (p.hasPermission("waypoints.go") || p.hasPermission("waypoints.plates")) {
-                        if (!s.getLine(i).equals("§aWaypoint:")) {
+                if (plugin.isBukkitPermissions())
+                {
+                    if (p.hasPermission("waypoints.go") || p.hasPermission("waypoints.plates"))
+                    {
+                        if (!s.getLine(i).equals("§aWaypoint:"))
+                        {
                             s.setLine(i, "§aWaypoint:");
                             s.update();
                         }
-                        p.teleport(plugin.getPlugin().getWaypoint(wp).getLocation());
+                        if (plugin.getPlugin().getConfigManager().costingEnabled())
+                        {
+                            int cost = Integer.parseInt(s.getLine(i + 2));
+                            if (s.getLine(i + 2) == "")
+                            {
+                                plugin.getPlugin().getCommandHandler().doGo(p, wp);
+                            } else
+                            {
+                                if (p.getLevel() < cost)
+                                {
+                                    p.sendMessage("You do not have the required levels to do that.");
+                                    return false;
+                                }
+                                p.setLevel(p.getLevel() - cost);
+                                plugin.getPlugin().getCommandHandler().doGoNoCost(p, wp);
+                            }
+                        } else
+                        {
+                            p.teleport(plugin.getPlugin().getWaypoint(wp).getLocation());
+                        }
                         return true;
-                    } else {
+                    } else
+                    {
                         p.sendMessage("You do not have permission to pressure plate waypoint.");
                         return false;
                     }
-                } else {
-                    if (!s.getLine(i).equals("§aWaypoint:")) {
+                } else
+                {
+                    if (!s.getLine(i).equals("§aWaypoint:"))
+                    {
                         s.setLine(i, "§aWaypoint:");
                         s.update();
                     }
-                    p.teleport(plugin.getPlugin().getWaypoint(wp).getLocation());
+                    if (plugin.getPlugin().getConfigManager().costingEnabled())
+                    {
+                        if (s.getLine(i + 2).equals(""))
+                        {
+                            plugin.getPlugin().getCommandHandler().doGo(p, wp);
+                        } else
+                        {
+                            int cost = Integer.parseInt(s.getLine(i + 2));
+                            if (p.getLevel() < cost)
+                            {
+                                p.sendMessage("You do not have the required levels to do that.");
+                                return false;
+                            }
+                            p.setLevel(p.getLevel() - cost);
+                            plugin.getPlugin().getCommandHandler().doGoNoCost(p, wp);
+                        }
+                    } else
+                    {
+                        p.teleport(plugin.getPlugin().getWaypoint(wp).getLocation());
+                    }
                     return true;
                 }
             }
